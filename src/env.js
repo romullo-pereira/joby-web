b6b0adb0047e006efe828518974ded93fb7abfe8
romullo-pereira <romulo.pereira@bandtec.com.br>
2021-09-06 19:49:54 -0300

    fix url api

--- src/env.js
+++ src/env.js
@@ -1,5 +1,6 @@
 // const baseUrl = "http://localhost:8080";
-const baseUrl = "http://ec2-user@ec2-54-90-87-70.compute-1.amazonaws.com:8080";
+const baseUrl = "http://ec2-user@ec2-54-164-173-81.compute-1.amazonaws.com:8080";
+
 
 /*
     Logout instituição
