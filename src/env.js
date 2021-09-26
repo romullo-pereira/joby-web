e39820d6c7194868e11f07a1d849390d4edabc64
michele-calazans <michele.calazans@bandtec.com.br>
2021-09-25 21:44:19 -0300

    feat: update url

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://localhost:8080";
-const baseUrl = "http://ec2-54-145-166-247.compute-1.amazonaws.com:8080";
+const baseUrl = "http://ec2-3-84-219-69.compute-1.amazonaws.com:8080";
 
 
 /*
